c5faaa4d3ab64e6adf90344c780635f87400946c
Sanyamdksh <sanyamdaksh19@gmail.com>
2025-11-06 00:45:25 +0530

    created isLoggedin middleware and set up some routes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const isLoggedin = require("../middlewares/isLoggedin");
 
 router.get("/", (req, res) => {
   res.render("index");
+});
+
+router.get("/shop", isLoggedin, (req, res) => {
+  res.render("shop");
+});
+
+router.get("/logout", isLoggedin, (req, res) => {
+  res.render("shop");
 });
 
 module.exports = router;
