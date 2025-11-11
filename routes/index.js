3b4a8a941894cea29c4ffdbc28ef943f0607349b
Sanyamdksh <sanyamdaksh19@gmail.com>
2025-11-11 17:55:04 +0530

    started working on the add to cart functionality

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,32 @@ const express = require("express");
 const router = express.Router();
 const isLoggedin = require("../middlewares/isLoggedin");
 const productModel = require("../models/product-model");
+const userModel = require("../models/user-model");
 
 router.get("/", (req, res) => {
-  res.render("index");
+  let error = req.flash("error");
+  res.render("index", { error, loggedin: false });
 });
 
 router.get("/shop", isLoggedin, async (req, res) => {
   let product = await productModel.find();
-  res.render("shop", { product });
+  let success = req.flash("success");
+  res.render("shop", { product, success });
+});
+
+router.get("/cart", isLoggedin, async (req, res) => {
+  let user = await userModel
+    .findOne({ email: req.user.email })
+    .populate("cart");
+  res.render("cart");
+});
+
+router.get("/addtocart/:productid", isLoggedin, async (req, res) => {
+  let user = await userModel.findOne({ email: req.user.email });
+  user.cart.push(req.params.productid);
+  await user.save();
+  req.flash("success", "Added to cart");
+  res.redirect("/shop");
 });
 
 router.get("/logout", isLoggedin, (req, res) => {
