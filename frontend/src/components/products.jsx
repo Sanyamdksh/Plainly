d6fdccfaea3701d9487208c23c3f0ce81ab3d1d4
Sanyamdksh <sanyamdaksh19@gmail.com>
2025-11-14 23:55:52 +0530

    tweaked the layout

--- frontend/src/components/products.jsx
+++ frontend/src/components/products.jsx
@@ -14,11 +14,23 @@ const Products = () => {
 
         <div className="grid grid-cols-3 mt-10 gap-6">
           {[1, 2, 3, 4].map((item) => (
-            <div key={item} className="bg-white rounded-2xl p-6">
-              <div className="h-48 w-auto bg-[#FFF37A] rounded-xl"></div>
+            <div
+              key={item}
+              className="bg-white rounded-2xl p-6 shadow-md hover:shadow-lg transition-all"
+            >
+              <div className="h-48 w-auto bg-[#FFF37A] rounded-xl shadow-sm"></div>
               <h3 className="text-xl font-medium text-stone-700 mt-2">
                 Product {item}
               </h3>
+              <p className="font-bold text-stone-900">Rs. 300</p>
+              <div className="flex flex-col items-center mt-5 gap-3">
+                <button className="bg-red-400 text-white font-medium py-2 rounded-lg w-3/4 hover:bg-red-500 transition">
+                  Buy Now
+                </button>
+                <button className="bg-stone-200 py-2 rounded-lg w-3/4 hover:bg-stone-300 transition">
+                  Add to Cart
+                </button>
+              </div>
             </div>
           ))}
         </div>
