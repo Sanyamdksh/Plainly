b995bbb0c017ed256f5e3312eeeb40f9184920be
Sanyamdksh <sanyamdaksh19@gmail.com>
2025-11-18 00:53:16 +0530

    Implemented the route to add products

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Products from "./components/products";
 import AuthContainer from "./pages/AuthContainer";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import DashLayout from "./pages/admin/dashLayout";
+import AddProduct from "./pages/admin/addProduct";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
           }
         />
         <Route path="/owner/dashboard" element={<DashLayout />} />
+        <Route path="/owner/add-product" element={<AddProduct />} />
       </Routes>
     </Router>
   );
--- frontend/src/pages/admin/addProduct.jsx
+++ frontend/src/pages/admin/addProduct.jsx
@@ -1,17 +1,67 @@
 import React, { useState } from "react";
+import Dashboard from "./dashboard";
+const AddProduct = () => {
+  // const [product, setProduct] = useState({
+  //   name: "",
+  //   price: "",
+  //   discount: 0,
+  //   bgcolor: "#ffffff",
+  //   panelcolour: "#ffffff",
+  //   textcolor: "#000000",
+  //   image: null,
+  // });
 
-const addProduct = () => {
-  const [product, setProduct] = useState({
-    name: "",
-    price: "",
-    discount: 0,
-    bgcolor: "#ffffff",
-    panelcolour: "#ffffff",
-    textcolor: "#000000",
-    image: null,
-  });
+  return (
+    <Dashboard>
+      <div className="max-w-2xl mx-auto">
+        <h2 className="text-2xl font-semibold text-stone-700 mb-6">
+          Add new product
+        </h2>
+        <div className="bg-white p-6 rounded-lg shadow-md max-w-lg space-y-5">
+          <input
+            type="text"
+            placeholder="Product name"
+            className="w-full border p-3 rounded-md"
+          />
+          <input
+            type="number"
+            placeholder="Price"
+            className="w-full border p-3 rounded-md"
+          />
+          <input
+            type="number"
+            placeholder="Discount"
+            className="w-full border p-3 rounded-md"
+          />
 
-  return <div></div>;
+          <div className="flex gap-4">
+            <label className="flex flex-col">
+              <span className="text-sm text-stone-600">Background Color</span>
+              <input type="color" className="w-16 h-10" />
+            </label>
+
+            <label className="flex flex-col">
+              <span className="text-sm text-stone-600">Panel Color</span>
+              <input type="color" className="w-16 h-10" />
+            </label>
+
+            <label className="flex flex-col">
+              <span className="text-sm text-stone-600">Text Color</span>
+              <input type="color" className="w-16 h-10" />
+            </label>
+          </div>
+          <input
+            type="file"
+            className="w-full bg-stone-100 p-3 border rounded-lg"
+          />
+
+          <button className="w-full bg-stone-800 text-white py-3 rounded-lg hover:bg-stone-900">
+            Add Product
+          </button>
+        </div>
+      </div>
+    </Dashboard>
+  );
 };
 
-export default addProduct;
+export default AddProduct;
